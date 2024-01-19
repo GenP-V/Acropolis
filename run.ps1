3f7fc53fef94b4cbda4ef285069705ce11353d1f
GenP-V <157014463+GenP-V@users.noreply.github.com>
2024-01-19 10:30:30 -0800

    Initial commit

---
+++ run.ps1
@@ -0,0 +1,29 @@
+# Guide available at https://www.reddit.com/r/GenP/
+
+$ErrorActionPreference = "Stop"
+# Enable TLSv1.2 for compatibility with older clients
+[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+
+$DownloadURL = 'https://raw.githubusercontent.com/GenP-V/Acropolis/main/Acropolis.cmd'
+
+$rand = Get-Random -Maximum 99999999
+$isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
+$FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\Acropolis_$rand.cmd" } else { "$env:TEMP\Acropolis_$rand.cmd" }
+
+try {
+    $response = Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing
+}
+catch {
+    Write-Error "Failed to download Acropolis.cmd from $DownloadURL"
+    exit 1
+}
+
+$ScriptArgs = "$args "
+$prefix = "@REM $rand `r`n"
+$content = $prefix + $response
+Set-Content -Path $FilePath -Value $content
+
+Start-Process $FilePath $ScriptArgs -Wait
+
+$FilePaths = @("$env:TEMP\Acropolis*.cmd", "$env:SystemRoot\Temp\Acropolis*.cmd")
+foreach ($FilePath in $FilePaths) { Get-Item $FilePath | Remove-Item }
