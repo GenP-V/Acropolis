f4d6e7a78288deae58103f149522c59e3db04307
GenP-V <157014463+GenP-V@users.noreply.github.com>
2024-02-12 14:45:29 -0800

    Update Acropolis.cmd

--- Acropolis.cmd
+++ Acropolis.cmd
@@ -7,6 +7,8 @@ cd /d "%~dp0"
 taskkill /IM powershell.exe /F
 
 title Acropolis V%ver%
+mode 82, 24
+set customAcrobatPath= "C:\Program Files\Adobe\Acrobat DC\Acrobat"
 
 :MainMenu
 cls
@@ -46,7 +48,6 @@ if %userChoice%==6 goto ExitScript
 goto MainMenu
 
 
-
 :DownloadInstall
 if not exist "%TEMP%\Acropolis" (
     md "%TEMP%\Acropolis"
@@ -59,7 +60,7 @@ echo:     ______________________________________________________________________
 echo.
 REM Download and Installation steps here
 rem Download the standalone Acrobat version needed
-curl --output "%TEMP%\Acropolis\Acrobat_DC_Web_x64_WWMUI.zip" https://trials.adobe.com/AdobeProducts/APRO/Acrobat_HelpX/win32/Acrobat_DC_Web_x64_WWMUI.zip
+curl --ssl-no-revoke --progress-bar --output "%TEMP%\Acropolis\Acrobat_DC_Web_x64_WWMUI.zip" https://trials.adobe.com/AdobeProducts/APRO/Acrobat_HelpX/win32/Acrobat_DC_Web_x64_WWMUI.zip
 rem Check if the download was successful
 if not exist "%TEMP%\Acropolis\Acrobat_DC_Web_x64_WWMUI.zip" (
     echo Error: Failed to download Adobe Acrobat DC. Please check your internet connection, disable your Antivirus and try again.
@@ -85,8 +86,7 @@ echo:
 echo:         Click the install button and wait for the installation to finish.
 echo:         ________________________________________________________________  
 echo:
-echo:        
-echo:
+echo:             Click the FINISH button at the end of the installation.
 echo:     ________________________________________________________________________
 echo.
 rem Run the setup file silently
@@ -106,7 +106,7 @@ echo:                                Downloading Patch...
 echo:     ________________________________________________________________________
 echo.
 REM Download the patch files
-curl -L --output "%TEMP%\Acropolis\AcrobatV.zip" y.gy/AcrobatV
+curl --ssl-no-revoke --progress-bar -L --output "%TEMP%\Acropolis\AcrobatV.zip" y.gy/AcrobatV
 echo.
 rem Check if the download was successful    
 if not exist "%TEMP%\Acropolis\AcrobatV.zip" (
@@ -148,14 +148,14 @@ echo:                        Creating backup of default files...
 echo:     ________________________________________________________________________
 echo.
 rem Create backup files
-if not exist "C:\Program Files\Adobe\Acrobat DC\Acrobat\acrotray.exe.bak" (
-    copy "C:\Program Files\Adobe\Acrobat DC\Acrobat\acrotray.exe" "C:\Program Files\Adobe\Acrobat DC\Acrobat\acrotray.exe.bak"
+if not exist "%customAcrobatPath%\acrotray.exe.bak" (
+    copy "%customAcrobatPath%\acrotray.exe" "%customAcrobatPath%\acrotray.exe.bak"
 )
-if not exist "C:\Program Files\Adobe\Acrobat DC\Acrobat\Acrobat.dll.bak" (
-    copy "C:\Program Files\Adobe\Acrobat DC\Acrobat\Acrobat.dll" "C:\Program Files\Adobe\Acrobat DC\Acrobat\Acrobat.dll.bak"
+if not exist "%customAcrobatPath%\Acrobat.dll.bak" (
+    copy "%customAcrobatPath%\Acrobat.dll" "%customAcrobatPath%\Acrobat.dll.bak"
 )
-if not exist "C:\Program Files\Adobe\Acrobat DC\Acrobat\acrodistdll.dll.bak" (
-    copy "C:\Program Files\Adobe\Acrobat DC\Acrobat\acrodistdll.dll" "C:\Program Files\Adobe\Acrobat DC\Acrobat\acrodistdll.dll.bak
+if not exist "%customAcrobatPath%\acrodistdll.dll.bak" (
+    copy "%customAcrobatPath%\acrodistdll.dll" "%customAcrobatPath%\acrodistdll.dll.bak"
 )
 echo.
 echo Backup created.
@@ -165,6 +165,7 @@ pause
 if %userChoice%==3 goto ExtraSubmenu
 goto MainMenu
 
+
 :PatchAcrobat
 cls
 echo:     ________________________________________________________________________
@@ -172,17 +173,16 @@ echo:
 echo:                             Patching Adobe Acrobat DC...
 echo:     ________________________________________________________________________
 echo.
-REM Patching steps here
-rem Replace the files inside Acrobat with the patched files.
-
-xcopy /y "%TEMP%\Acropolis\acrotray.exe" "C:\Program Files\Adobe\Acrobat DC\Acrobat\acrotray.exe"
-xcopy /y "%TEMP%\Acropolis\Acrobat.dll" "C:\Program Files\Adobe\Acrobat DC\Acrobat\Acrobat.dll"
-xcopy /y "%TEMP%\Acropolis\acrodistdll.dll" "C:\Program Files\Adobe\Acrobat DC\Acrobat\acrodistdll.dll"
+rem Patching steps here, using customPath
+xcopy /y "%TEMP%\Acropolis\acrotray.exe" "%customAcrobatPath%\acrotray.exe"
+xcopy /y "%TEMP%\Acropolis\Acrobat.dll" "%customAcrobatPath%\Acrobat.dll"
+xcopy /y "%TEMP%\Acropolis\acrodistdll.dll" "%customAcrobatPath%\acrodistdll.dll"
 rmdir /s /q "%TEMP%\Acropolis"
 echo.
 echo Files patched and replaced.
 echo.
 goto DisableAdobeUpdater
+
 
 :DisableAdobeUpdater
 cls
@@ -211,10 +211,10 @@ echo:                        Disabling Adobe Crash Processor...
 echo:     ________________________________________________________________________
 echo.
 rem Create backup files and disable Adobe Crash Processor
-if not exist "C:\Program Files\Adobe\Acrobat DC\Acrobat\Adobe Crash Processor.exe.bak" (
-    copy "C:\Program Files\Adobe\Acrobat DC\Acrobat\Adobe Crash Processor.exe" "C:\Program Files\Adobe\Acrobat DC\Acrobat\Adobe Crash Processor.exe.bak"
+if not exist "%customAcrobatPath%\Adobe Crash Processor.exe.bak" (
+    copy "%customAcrobatPath%\Adobe Crash Processor.exe" "%customAcrobatPath%\Adobe Crash Processor.exe.bak"
 )
-del /f "C:\Program Files\Adobe\Acrobat DC\Acrobat\Adobe Crash Processor.exe"
+del /f "%customAcrobatPath%\Adobe Crash Processor.exe"
 echo.
 echo Adobe Crash Processor disabled.
 
@@ -283,10 +283,10 @@ echo:                           Disabling AdobeCollabSync...
 echo:     ________________________________________________________________________
 echo.
 rem Disable AdobeCollabSync
-if not exist "C:\Program Files\Adobe\Acrobat DC\Acrobat\AdobeCollabSync.exe.bak" (
-    copy "C:\Program Files\Adobe\Acrobat DC\Acrobat\AdobeCollabSync.exe" "C:\Program Files\Adobe\Acrobat DC\Acrobat\AdobeCollabSync.exe.bak"
+if not exist "%customAcrobatPath%\AdobeCollabSync.exe.bak" (
+    copy "%customAcrobatPath%\AdobeCollabSync.exe" "%customAcrobatPath%\AdobeCollabSync.exe.bak"
 )
-del /f "C:\Program Files\Adobe\Acrobat DC\Acrobat\AdobeCollabSync.exe"
+del /f "%customAcrobatPath%\AdobeCollabSync.exe"
 echo.
 echo AdobeCollabSync disabled.
 echo.
@@ -302,14 +302,14 @@ echo:                       Restoring backup of default files...
 echo:     ________________________________________________________________________
 echo.
 rem Restore backup files
-if exist "C:\Program Files\Adobe\Acrobat DC\Acrobat\acrotray.exe.bak" (
-    copy "C:\Program Files\Adobe\Acrobat DC\Acrobat\acrotray.exe.bak" "C:\Program Files\Adobe\Acrobat DC\Acrobat\acrotray.exe"
+if exist "%customAcrobatPath%\acrotray.exe.bak" (
+    copy "%customAcrobatPath%\acrotray.exe.bak" "%customAcrobatPath%\acrotray.exe"
 )
-if exist "C:\Program Files\Adobe\Acrobat DC\Acrobat\Acrobat.dll.bak" (
-    copy "C:\Program Files\Adobe\Acrobat DC\Acrobat\Acrobat.dll.bak" "C:\Program Files\Adobe\Acrobat DC\Acrobat\Acrobat.dll"
+if exist "%customAcrobatPath%\Acrobat.dll.bak" (
+    copy "%customAcrobatPath%\Acrobat.dll.bak" "%customAcrobatPath%\Acrobat.dll"
 )
-if exist "C:\Program Files\Adobe\Acrobat DC\Acrobat\acrodistdll.dll.bak" (
-    copy "C:\Program Files\Adobe\Acrobat DC\Acrobat\acrodistdll.dll.bak" "C:\Program Files\Adobe\Acrobat DC\Acrobat\acrodistdll.dll"
+if exist "%customAcrobatPath%\acrodistdll.dll.bak" (
+    copy "%customAcrobatPath%\acrodistdll.dll.bak" "%customAcrobatPath%\acrodistdll.dll"
 )
 echo Backup restored.
 pause
@@ -324,10 +324,10 @@ echo:                        Re-enabling Adobe Crash Processor...
 echo:     ________________________________________________________________________
 echo.
 rem Restore Adobe Crash Processor
-if exist "C:\Program Files\Adobe\Acrobat DC\Acrobat\Adobe Crash Processor.exe.bak" (
-    copy "C:\Program Files\Adobe\Acrobat DC\Acrobat\Adobe Crash Processor.exe.bak" "C:\Program Files\Adobe\Acrobat DC\Acrobat\Adobe Crash Processor.exe"
+if exist "%customAcrobatPath%\Adobe Crash Processor.exe.bak" (
+    copy "%customAcrobatPath%\Adobe Crash Processor.exe.bak" "%customAcrobatPath%\Adobe Crash Processor.exe"
 )
-del /f "C:\Program Files\Adobe\Acrobat DC\Acrobat\Adobe Crash Processor.exe.bak"
+del /f "%customAcrobatPath%\Adobe Crash Processor.exe.bak"
 echo Adobe Crash Processor re-enabled.
 echo.
 pause
@@ -341,10 +341,10 @@ echo:                           Re-enabling AdobeCollabSync...
 echo:     ________________________________________________________________________
 echo.
 rem Restore AdobeCollabSync
-if exist "C:\Program Files\Adobe\Acrobat DC\Acrobat\AdobeCollabSync.exe.bak" (
-    copy "C:\Program Files\Adobe\Acrobat DC\Acrobat\AdobeCollabSync.exe.bak" "C:\Program Files\Adobe\Acrobat DC\Acrobat\AdobeCollabSync.exe"
+if exist "%customAcrobatPath%\AdobeCollabSync.exe.bak" (
+    copy "%customAcrobatPath%\AdobeCollabSync.exe.bak" "%customAcrobatPath%\AdobeCollabSync.exe"
 )
-del /f "C:\Program Files\Adobe\Acrobat DC\Acrobat\AdobeCollabSync.exe.bak"
+del /f "%customAcrobatPath%\AdobeCollabSync.exe.bak"
 echo.
 echo AdobeCollabSync re-enabled.
 echo.
@@ -385,21 +385,48 @@ echo:         [1] Create backup of default files
 echo:         [2] Disable Adobe Updater
 echo:         [3] Disable Adobe Crash Processor
 echo:         [4] Disable AdobeCollabSync (Not included, may cause issues!)
+echo:         [5] Set Custom Acrobat Installation Path
 echo:
 echo:         [0] Return to Main Menu
 echo:     ________________________________________________________________________
 echo.
-echo:     Enter a menu option in the Keyboard [1,2,3,4,0] :
-choice /C:12340 /N
+echo:     Enter a menu option in the Keyboard [1,2,3,4,5,0] :
+choice /C:123450 /N
 set "extraChoice=%errorlevel%"
 
 if %extraChoice%==1 goto BackupFiles
 if %extraChoice%==2 goto DisableAdobeUpdater
 if %extraChoice%==3 goto DisableBackgroundServices
 if %extraChoice%==4 goto DisableCollabSync
-if %extraChoice%==5 goto MainMenu
+if %extraChoice%==5 goto SetCustomAcrobatPath
+if %extraChoice%==6 goto MainMenu
 
-goto MainMenu
+goto ExtraSubmenu
+
+:SetCustomAcrobatPath
+cls
+title Acropolis - Custom Path
+echo:     ________________________________________________________________________
+echo:
+echo:                           Custom Acrobat Installation Path
+echo:     ________________________________________________________________________
+echo.
+echo:     Please enter the custom installation path for Adobe Acrobat 
+echo:     (e.g., D:\Adobe\Acrobat DC\Acrobat):
+echo.
+echo:     Current path: %customAcrobatPath%
+echo:     ________________________________________________________________________
+echo. 
+set /p customAcrobatPath=".    Enter path: "
+if "%customAcrobatPath%"=="" goto ExtraSubmenu
+echo.
+echo      New path set to: %customAcrobatPath%
+echo:     ________________________________________________________________________
+echo.
+echo:     Press any key to continue...
+
+pause > nul
+goto ExtraSubmenu
 
 
 :RestoreDefaultsSubmenu
